5ceb95eedaabcaf42b4aa2d2ae0422a261379563
dgbragas <dgbragas.dev@outlook.com>
2020-12-01 13:03:09 -0300

    feat: adding the ThemeProvider in <App /> to allow all components access the global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { ThemeProvider, Theme } from 'styled-components';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+import DefaultTheme from './themes/default';
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => {
+  const theme: Theme = DefaultTheme;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+};
 
 export default App;
